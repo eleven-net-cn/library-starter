849ab53d41dfeb1f2cb8cfbaafe4a9e3adbe7464
Eleven <master@eleven.net.cn>
2021-06-01 11:47:46 +0800

    ci: update babel config

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,23 @@
 module.exports = {
+  loaderOptions: {
+    /**
+     * Babel 编译时，会处理 core-js（未来可能会被修复），
+     * 导致 polyfill 内部代码发生了变化，产生一些微小的影响，如 Symbol 问题。
+     * 暂时我们手动声明略过。
+     * https://github.com/zloirock/core-js/issues/514
+     * https://github.com/rails/webpacker/pull/2031
+     */
+    exclude: [/node_modules[\\/]core-js/],
+  },
+  assumptions: {
+    /**
+     * https://babeljs.io/docs/en/assumptions#setpublicclassfields
+     *
+     * 装饰器的 legancy: true，依赖此配置
+     *  - https://babeljs.io/docs/en/babel-plugin-proposal-decorators#legacy
+     */
+    setPublicClassFields: true,
+  },
   presets: [
     [
       '@babel/preset-env',
@@ -27,11 +46,6 @@ module.exports = {
         legacy: true, // 推荐
       },
     ],
-    [
-      '@babel/plugin-proposal-class-properties',
-      {
-        loose: true, // babel 编译时，对 class 的属性采用赋值表达式，而不是 Object.defineProperty（更简洁）
-      },
-    ],
+    ['@babel/plugin-proposal-class-properties'],
   ],
 };
